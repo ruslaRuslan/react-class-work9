90dbbec6d551bdeb71adefb295fdc11f5d253fd8
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-22 14:07:29 +0400

    axios

--- src/page/UsersWithReducer/index.jsx
+++ src/page/UsersWithReducer/index.jsx
@@ -1,8 +1,11 @@
+import axios from "axios"
 import { useEffect } from "react"
+const url = 'https://jsonplaceholder.typicode.com/users/'
+
 
 const UsersWithReducerPage = ({}) => {
   useEffect(()=>{
-
+axios.get(url)
   },[])
   return (
     <div>UsersWithReducerPage</div>
