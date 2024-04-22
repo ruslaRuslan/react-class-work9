376a7662501dac73b68ff9dc8da90c90d71ff43a
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-22 16:19:02 +0400

    useState, useEffect hazir

--- src/page/UsersWithReducer/index.jsx
+++ src/page/UsersWithReducer/index.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 const url = "https://jsonplaceholder.typicode.com/users/";
 
-const UsersWithReducerPage = ({}) => {
+const UsersWithStatePage = ({}) => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -26,22 +26,26 @@ const UsersWithReducerPage = ({}) => {
     setUsers(users.filter((user) => user.id !== id));
   };
 
-  const onAddUser = ()=>{
-    setUsers([...users, {id:+users.length+1, username:prompt('yeni user elave et')}])
-  }
+  const onAddUser = () => {
+    setUsers([
+      ...users,
+      { id: +users.length + 1, username: prompt("yeni user elave et") },
+    ]);
+  };
 
-  const onDeleteLast =()=>{
-    setUsers(users.slice(0, users.length - 1))
-  }
+  const onDeleteLast = () => {
+    setUsers(users.slice(1));
+  };
+
+  const onDeleteFirst = () => {
+    setUsers(users.slice(1));
+  };
 
   return (
     <div>
-      <button
-      onClick={onDeleteLast}
-      >delete Last</button>
-      <button
-      onClick={onAddUser}
-      >user elave et</button>
+      <button onClick={onDeleteFirst}>delete first</button>
+      <button onClick={onDeleteLast}>delete Last</button>
+      <button onClick={onAddUser}>user elave et</button>
       {users.map(({ id, username }) => {
         return (
           <div key={id}>
@@ -69,4 +73,4 @@ const UsersWithReducerPage = ({}) => {
   );
 };
 
-export default UsersWithReducerPage;
+export default UsersWithStatePage;
